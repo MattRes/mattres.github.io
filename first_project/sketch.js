3000cc2901959ded71f2304fc1909753f62f6fce
MattRes <44241122+MattRes@users.noreply.github.com>
2019-09-06 14:11:19 -0600

    Update sketch.js

--- first_project/sketch.js
+++ first_project/sketch.js
@@ -5,15 +5,31 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let redamount = 0;
+let greenamount = 0;
+let redchangeamount = 1;
+let greenchangeamount = 1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(0);
 }
 
 function draw() {
-  background(220);
-  fill(0);
-  ell
-  ellipse(mouseX, mouseY, 1000, 100);
+  fill(redamount, greenamount, 255);
+  noStroke()
+  ellipse(mouseX, mouseY, 100, 100);
+
+  redamount += redchangeamount;
+
+  if (redamount >= 255){
+    redchangeamount *= -1;
+    greenchangeamount *= -2;
+  }
+  if (redamount <= 0) {
+    redchangeamount *= -1;
+    greenchangeamount *= -2;
+  }
+  console.log(redamount);
 }
 
